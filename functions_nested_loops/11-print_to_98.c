d301f35576e72c1caec0ca4b38c31c2d0cf5b41a
Ali-tech13 <3looi3445@gmail.com>
2026-07-08 02:59:47 +0300

    Write function that print to 98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -11,13 +11,13 @@ void print_to_98(int n)
 {
 while (n > 98)
 {
-printf("%d\n", n--);
-printf(", ");
+printf("%d, ", n);
+n--;
 }
 while (n < 98)
 {
-printf("%d\n", n++);
-printf(", ");
+printf("%d, ", n);
+n++;
 }
 printf("%d\n", n);
 }
