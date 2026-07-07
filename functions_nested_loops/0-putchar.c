450152030a1fa9019369b122e869ad2264d468f9
Ali-tech13 <3looi3445@gmail.com>
2026-07-07 17:57:23 +0300

    Edit file

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,3 +1,4 @@
+#include "main.h"
 /**
 * main - Print _putchar
 *
@@ -5,7 +6,13 @@
 **/
 int main(void)
 {
-_putchar('_')
-_putchar
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 return (0);
 }
