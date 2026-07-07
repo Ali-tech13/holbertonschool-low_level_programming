f266e7cdf4edbc2b77c351326014e84fa2c39fb3
Ali-tech13 <3looi3445@gmail.com>
2026-07-08 01:04:06 +0300

    Write a function to get the abs value of a number

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* _abs - calculate the abs value of an intger
+* @n: number to check
+*
+* Return: Abs value of n
+**/
+int _abs(int n)
+{
+if (n < 0)
+{
+return (-n);
+}
+return (n);
+}
