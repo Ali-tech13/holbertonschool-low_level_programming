56963948b224464135a393a7c7d833e49b6461d4
Ali-tech13 <3looi3445@gmail.com>
2026-07-09 19:12:09 +0300

    Write function game

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,39 +1,30 @@
-#include "main.h"
 #include <stdio.h>
+
 /**
-* main - program that prints the numbers from 1 to 100,
-* followed by a new line.
-*
-*
-*
-* Return: void
-*
-**/
+ * main - prints the numbers from 1 to 100
+ *
+ * Return: 0
+ */
 int main(void)
 {
-int i;
-for (i = 1; i <= 100; i++)
-{
-if (i % 3 == 0 && i % 5 == 0)
-{
-printf("FizzBuzz");
-}
-else if (i % 3 == 0)
-{
-printf("Fizz");
-}
-else if (i % 5 == 0)
-{
-printf("Buzz");
-}
-else
-{
-printf("%d ", i);
-}
-if (i != 100)
-{
-printf(" ");
-}
-}
-return (0);
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+
+		if (i != 100)
+			printf(" ");
+	}
+
+	printf("\n");
+
+	return (0);
 }
