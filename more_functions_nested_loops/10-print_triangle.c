ae581ad2be763c89f11bee88356d27abf53b7e9f
Ali-tech13 <3looi3445@gmail.com>
2026-07-09 19:23:31 +0300

    Write a function to triangle

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -10,9 +10,19 @@ void print_triangle(int size)
 {
 int i;
 int j;
-for (i = 0; i < size; i++)
+int k;
+if (size <= 0)
 {
-for (j = 0; j < i; j++)
+_putchar('\n');
+return;
+}
+for (i = 0; i <= size; i++)
+{
+for (j = 0; j < size - i; j++)
+{
+_putchar('#');
+}
+for (k = 0; k < i; k++)
 {
 _putchar('#');
 }
