50da862c5adf54a8148e1e0951a588a67b9ec78b
Ali-tech13 <3looi3445@gmail.com>
2026-07-09 18:05:20 +0300

    Write a function to print size

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* print_square - function that prints a square, followed by a new line.
+* @size: size to consider
+*
+*
+* Return: void
+*
+**/
+void print_square(int size)
+{
+int i;
+int j;
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
