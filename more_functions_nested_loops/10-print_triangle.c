eaa6fc1d2accf1ebba2f2030de3f9ebbb7393c67
Ali-tech13 <3looi3445@gmail.com>
2026-07-09 19:25:44 +0300

    Write a function to triangle

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -16,11 +16,11 @@ if (size <= 0)
 _putchar('\n');
 return;
 }
-for (i = 0; i <= size; i++)
+for (i = 1; i <= size; i++)
 {
 for (j = 0; j < size - i; j++)
 {
-_putchar('#');
+_putchar(' ');
 }
 for (k = 0; k < i; k++)
 {
