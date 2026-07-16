e77cb55679b60c95f41f13712120fa6b6aee95d6
Ali-tech13 <3looi3445@gmail.com>
2026-07-16 19:53:03 +0300

    Write a function to check if char is found

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stddef.h>
+/**
+*_strpbrk - function that searches a string for any of a set of bytes.
+*
+* @s: pointer to array
+* @accept: pointer to araay
+*
+* Return: null if not found - addres otherwise
+*
+**/
+char *_strpbrk(char *s, char *accept)
+{
+int i;
+int j;
+int len = 0;
+int addres = 0;
+int found;
+while (s[len] != '\0')
+{
+len++;
+}
+for (i = 0; i < len; i++)
+{
+found = 0;
+for (j = 0; accept[j] != '\0'; j++)
+{
+if (s[i] == accept[j])
+{
+return (&s[i]);
+}
+}
+}
+return (NULL);
+}
