117eccb9c594730c339abc0a7a4f24731534d338
Ali-tech13 <3looi3445@gmail.com>
2026-07-16 19:30:44 +0300

    Write a function to find a char

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -10,24 +10,36 @@
 **/
 unsigned int _strspn(char *s, char *accept)
 {
-int i;
-int j;
-int count = 0;
-int len = 0;
-while (s[len] != '\0')
-{
-len++;
-}
-for (i = 0; i <= len; i++)
-{
-for (j = 0; accept[j] != '\0'; j++)
-{
-if (s[i] == accept[j])
-{
-count++;
-}
-}
-break;
-}
-return (count);
+	int i;
+	int j;
+	int count = 0;
+	int len = 0;
+	int found;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		found = 0;
+
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				found = 1;
+				count++;
+				break;
+			}
+		}
+
+		if (found == 0)
+		{
+			break;
+		}
+	}
+
+	return (count);
 }
