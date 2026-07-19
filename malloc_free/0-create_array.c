69b2551fc4b0abe00a479060c857f499a51aa70c
Ali-tech13 <3looi3445@gmail.com>
2026-07-19 19:33:18 +0300

    Write a function to fill an array with char

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+* create_array - function that creates an array of chars, and initializes it
+* with a specific char.
+*
+* @size: size of array
+* @c: char to write
+* Return: always char
+**/
+char *create_array(unsigned int size, char c)
+{
+int i;
+if (size == 0)
+{
+return (NULL);
+}
+char *arr = malloc(size * sizeof(char));
+if (arr == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < size; i++)
+{
+arr[i] = c;
+}
+return (arr);
+}
