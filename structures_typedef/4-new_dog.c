48a2ba4c3503c892a2b06d60e25d3551815a4a9a
Ali-tech13 <3looi3445@gmail.com>
2026-07-21 19:46:35 +0300

    Write a function that creates a new dog.

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,53 +1,61 @@
 #include "dog.h"
-#include "main.h"
 #include <stdlib.h>
+
 /**
-* *new_dog - function that creates a new dog.
-*
-*@name: name of dog
-*@age: age of dog
-*@owner: owner of dog
-*
-* Return: dog_t
-**/
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: pointer to the new dog, or NULL on failure
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-int i;
-char *newName;
-char *newOwner;
-int newAge;
-dog_t *dog;
-int len = 0;
-int len1 = 0;
-dog = malloc(sizeof(dog_t));
-while (name[len] != '\0')
-{
-len++;
-}
-while (owner[len1] != '\0')
-{
-len1++;
-}
-newName = malloc((len + 1) * sizeof(char));
+	dog_t *dog;
+	char *new_name;
+	char *new_owner;
+	int name_len = 0;
+	int owner_len = 0;
+	int i;
 
-for (i = 0; i < len; i++)
-{
-newName[i] = name[i];
-}
-newName[i] = '\0';
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
 
-newOwner = malloc((len1 + 1) * sizeof(char));
+	while (name[name_len] != '\0')
+		name_len++;
 
-for (i = 0; i < len1; i++)
-{
-newOwner[i] = owner[i];
-}
-newOwner[i] = '\0';
+	new_name = malloc((name_len + 1) * sizeof(char));
+	if (new_name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
 
-dog->name = newName;
-dog->owner = newOwner;
-dog->age = newAge;
+	for (i = 0; i < name_len; i++)
+		new_name[i] = name[i];
 
+	new_name[i] = '\0';
 
-return (dog);
+	while (owner[owner_len] != '\0')
+		owner_len++;
+
+	new_owner = malloc((owner_len + 1) * sizeof(char));
+	if (new_owner == NULL)
+	{
+		free(new_name);
+		free(dog);
+		return (NULL);
+	}
+
+	for (i = 0; i < owner_len; i++)
+		new_owner[i] = owner[i];
+
+	new_owner[i] = '\0';
+
+	dog->name = new_name;
+	dog->age = age;
+	dog->owner = new_owner;
+
+	return (dog);
 }
