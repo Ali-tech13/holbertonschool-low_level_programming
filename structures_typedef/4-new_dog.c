dc42fcc4210e67d87ec3c12d25355e5232cd6aad
Ali-tech13 <3looi3445@gmail.com>
2026-07-21 19:49:24 +0300

    Write a function that creates a new dog.

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -11,51 +11,42 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog;
-	char *new_name;
-	char *new_owner;
-	int name_len = 0;
-	int owner_len = 0;
-	int i;
+dog_t *dog;
+char *new_name;
+char *new_owner;
+int name_len = 0;
+int owner_len = 0;
+int i;
 
-	dog = malloc(sizeof(dog_t));
-	if (dog == NULL)
-		return (NULL);
+dog = malloc(sizeof(dog_t));
+if (dog == NULL)
+return (NULL);
+while (name[name_len] != '\0')
+name_len++;
+new_name = malloc((name_len + 1) * sizeof(char));
+if (new_name == NULL)
+{
+free(dog);
+return (NULL);
+}
+for (i = 0; i < name_len; i++)
+new_name[i] = name[i];
+new_name[i] = '\0';
+while (owner[owner_len] != '\0')
+owner_len++;
+new_owner = malloc((owner_len + 1) * sizeof(char));
+if (new_owner == NULL)
+{
+free(new_name);
+free(dog);
+return (NULL);
+}
+for (i = 0; i < owner_len; i++)
+	new_owner[i] = owner[i];
 
-	while (name[name_len] != '\0')
-		name_len++;
-
-	new_name = malloc((name_len + 1) * sizeof(char));
-	if (new_name == NULL)
-	{
-		free(dog);
-		return (NULL);
-	}
-
-	for (i = 0; i < name_len; i++)
-		new_name[i] = name[i];
-
-	new_name[i] = '\0';
-
-	while (owner[owner_len] != '\0')
-		owner_len++;
-
-	new_owner = malloc((owner_len + 1) * sizeof(char));
-	if (new_owner == NULL)
-	{
-		free(new_name);
-		free(dog);
-		return (NULL);
-	}
-
-	for (i = 0; i < owner_len; i++)
-		new_owner[i] = owner[i];
-
-	new_owner[i] = '\0';
-
-	dog->name = new_name;
-	dog->age = age;
-	dog->owner = new_owner;
-
-	return (dog);
+new_owner[i] = '\0';
+dog->name = new_name;
+dog->age = age;
+dog->owner = new_owner;
+return (dog);
 }
