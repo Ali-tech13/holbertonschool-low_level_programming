49b67c7bf20090d28269d43051a880187ca3df89
Ali-tech13 <3looi3445@gmail.com>
2026-07-22 08:26:40 +0300

    Write a function that free struct dog

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include "dog.h"
+#include <stdlib.h>
+/**
+* free_dog - function that frees dogs.
+*
+* @d: pointer to struct dog
+*
+*
+* Return: void
+*
+**/
+void free_dog(dog_t *d)
+{
+if (d == NULL)
+{
+return;
+}
+free(d->name);
+free(d->owner);
+free(d);
+}
