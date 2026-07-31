0e95b0309d32c8bd0b5b8bdc5c3c637fbf84974a
Ali-tech13 <3looi3445@gmail.com>
2026-07-31 05:26:30 +0300

    Write a function that print all

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,98 +1,72 @@
 #include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
-
 /**
- * print_char - prints a char
- * @args: variadic arguments
+ * print_char - prints a character
+ * @args: list of arguments
  */
 void print_char(va_list args)
 {
 	printf("%c", va_arg(args, int));
 }
-
 /**
  * print_int - prints an integer
- * @args: variadic arguments
+ * @args: list of arguments
  */
 void print_int(va_list args)
 {
 	printf("%d", va_arg(args, int));
 }
-
 /**
  * print_float - prints a float
- * @args: variadic arguments
+ * @args: list of arguments
  */
 void print_float(va_list args)
 {
 	printf("%f", va_arg(args, double));
 }
-
 /**
  * print_string - prints a string
- * @args: variadic arguments
+ * @args: list of arguments
  */
 void print_string(va_list args)
 {
 	char *str;
 
 	str = va_arg(args, char *);
-
 	if (str == NULL)
 		str = "(nil)";
-
 	printf("%s", str);
 }
-
 /**
- * print_all - prints anything
+ * print_all - prints arguments of different types
  * @format: list of argument types
  */
 void print_all(const char * const format, ...)
 {
-	int i;
-	int j;
-	char *separator;
+	int i = 0, j;
+	char *separator = "";
+	char *types = "cifs";
 	va_list args;
-
-	typedef struct printer
-	{
-		char symbol;
-		void (*print)(va_list);
-	} printer_t;
-
-	printer_t printers[] = {
-		{'c', print_char},
-		{'i', print_int},
-		{'f', print_float},
-		{'s', print_string}
+	void (*printers[])(va_list) = {
+		print_char, print_int, print_float, print_string
 	};
-
-	i = 0;
-	separator = "";
-
 	va_start(args, format);
-
 	while (format != NULL && format[i] != '\0')
 	{
 		j = 0;
-
-		while (j < 4)
+		while (types[j] != '\0')
 		{
-			if (format[i] == printers[j].symbol)
+			if (format[i] == types[j])
 			{
 				printf("%s", separator);
-				printers[j].print(args);
+				printers[j](args);
 				separator = ", ";
 			}
-
 			j++;
 		}
-
 		i++;
 	}
-
 	va_end(args);
 	printf("\n");
 }
