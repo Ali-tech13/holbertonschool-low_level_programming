e09a61c78c26e3756c32e15afd5a4ac952bc3588
Ali-tech13 <3looi3445@gmail.com>
2026-08-06 18:48:30 +0300

    Add file

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,33 +1,36 @@
 #include "lists.h"
 #include <stdlib.h>
-#include <stdio.h>
 #include <string.h>
+
 /**
-* add_node - adds a new node at the beginning of a list
-* @head: pointer to the head of the list
-* @str: string to duplicate into the new node
-*
-* Return: address of the new node, or NULL on failure
-*
-**/
+ * add_node - adds a new node at the beginning of a list
+ * @head: pointer to the head of the list
+ * @str: string to duplicate
+ *
+ * Return: address of the new node, or NULL on failure
+ */
 list_t *add_node(list_t **head, const char *str)
 {
-list_t *new_node;
-new_node = malloc(sizeof(list_t));
-if (new_node == NULL)
-return (NULL);
-new_node->str = strdup(str);
-if (new_node == NULL)
-return (NULL);
-new_node->str = strdup(str);
-if (new_node->str == NULL)
-{
-	free(new_node);
-	return (NULL);
-}
-new_node->len = strlen(str);
-new_node->next = *head;
-*head = new_node;
+	list_t *new_node;
+	unsigned int len = 0;
 
-return (new_node);
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	while (str[len] != '\0')
+		len++;
+
+	new_node->len = len;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
 }
