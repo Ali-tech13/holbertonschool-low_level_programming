e115f0da9b731e89ee0a1e7f0de61919577b2a7a
Ali-tech13 <3looi3445@gmail.com>
2026-08-10 01:55:59 +0300

    Add file

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+* free_dlistint - Function to free dlistint_t
+*
+*
+* @head: pointer to the first node
+* Return - void
+**/
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+
+}
