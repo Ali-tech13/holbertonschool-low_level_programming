bc5bb4940a10f203760b27297c6002ba8462f83b
Ali-tech13 <3looi3445@gmail.com>
2026-08-10 19:47:53 +0300

    Add file

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,59 +1,58 @@
-#include "lists.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include "lists.h"
+
 /**
-* insert_dnodeint_at_index - inserts a new node at a given position
-*
-* @h: address of the head pointer
-*
-* @idx: index where the new node will be added
-* @n: value for the new node
-* Return: address of new node, or NULL if it failed
-**/
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+ * insert_dnodeint_at_index - inserts a new node at a given position
+ * @h: address of the head pointer
+ * @idx: index where the new node should be added
+ * @n: value for the new node
+ *
+ * Return: address of the new node, or NULL if it failed
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h,
+				     unsigned int idx, int n)
 {
-	struct dlistint_s *tmp;
-	struct dlistint_s *newNode = malloc(sizeof(dlistint_t));
+	dlistint_t *tmp;
+	dlistint_t *newNode;
 	unsigned int len = 0;
 	unsigned int count = 0;
 
-	if (newNode == NULL)
-	{
+	if (h == NULL)
 		return (NULL);
-	}
-	tmp = (*h);
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	tmp = *h;
 	while (tmp != NULL)
 	{
 		tmp = tmp->next;
 		len++;
 	}
-	if (idx == 0)
-	{
-		add_dnodeint(h,n);
-	}
+
+	if (idx > len)
+		return (NULL);
 
 	if (idx == len)
-	{
-		add_dnodeint_end(h,n);
-	}
-	if (idx > len)
-	{
-		return (NULL);
-	}
-	newNode->n = n;
-	newNode->next = NULL;
-	newNode->prev = NULL;
-	tmp = *h;
+		return (add_dnodeint_end(h, n));
 
-	while (idx != count)
+	newNode = malloc(sizeof(dlistint_t));
+	if (newNode == NULL)
+		return (NULL);
+
+	tmp = *h;
+	while (count < idx)
 	{
 		tmp = tmp->next;
 		count++;
 	}
+
+	newNode->n = n;
 	newNode->next = tmp;
 	newNode->prev = tmp->prev;
+
 	tmp->prev->next = newNode;
 	tmp->prev = newNode;
-	return (newNode);
 
+	return (newNode);
 }
