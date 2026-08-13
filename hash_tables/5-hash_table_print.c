f96a1b9e4e58bf381ff6368d69948222e8c19133
Ali-tech13 <3looi3445@gmail.com>
2026-08-13 18:28:50 +0300

    Add file

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -24,7 +24,7 @@ void hash_table_print(const hash_table_t *ht)
 		{
 			if (first == 0)
 			printf(", ");
-			printf("'%s : '%s'", tmp->key, tmp->value);
+			printf("'%s' : '%s'", tmp->key, tmp->value);
 			first = 0;
 			tmp = tmp->next;
 		}
